8d7cb71d040af5e994fa0b80324c54d2b3e02cdc
kshitij <iamkshitij@live.com>
2019-08-18 11:22:33 +0530

    fix

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/chatbot-angular'));
+app.use(express.static(__dirname + '/dist/chatbot'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/chatbot-angular/index.html'));
+res.sendFile(path.join(__dirname+'/dist/chatbot/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
