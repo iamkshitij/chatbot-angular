fe4fcb4a9a1063d8dd3ad1a134c340f153e2cc05
kshitij <iamkshitij@live.com>
2019-08-18 11:18:58 +0530

    fix

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static(__dirname + '/dist/chatbot-angular'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/chatbot-angular<name-of-app>/index.html'));
+res.sendFile(path.join(__dirname+'/dist/chatbot-angular/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
